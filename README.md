66c41d27a34f44dd441516c82501c9befbbf72d4
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2022-02-04 11:43:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Play Store Bot
+A telegram play store search bot
+
+---
+
+## Variables
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+
+---
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import play_scraper
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+
+Bot = Client(
+    "Play-Store-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+@Bot.on_message(filters.private & filters.all)
+async def filter_all(bot, update):
+    text = "Search play store apps using below buttons.\n\nMade by @i_am_albin_praveen\n\nSupport @musicwithalby"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
+        ]
+    )
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_inline_query()
+async def search(bot, update):
+    results = play_scraper.search(update.query)
+    answers = []
+    for result in results:
+        details = "**Title:** `{}`".format(result["title"]) + "\n" \
+        "**Description:** `{}`".format(result["description"]) + "\n" \
+        "**App ID:** `{}`".format(result["app_id"]) + "\n" \
+        "**Developer:** `{}`".format(result["developer"]) + "\n" \
+        "**Developer ID:** `{}`".format(result["developer_id"]) + "\n" \
+        "**Score:** `{}`".format(result["score"]) + "\n" \
+        "**Price:** `{}`".format(result["price"]) + "\n" \
+        "**Full Price:** `{}`".format(result["full_price"]) + "\n" \
+        "**Free:** `{}`".format(result["free"]) + "\n" \
+        "\n" + "Made by @FayasNoushad"
+        reply_markup = InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="Play Store", url="https://play.google.com"+result["url"])]]
+        )
+        try:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=result["title"],
+                    description=result.get("description", None),
+                    thumb_url=result.get("icon", None),
+                    input_message_content=InputTextMessageContent(
+                        message_text=details, disable_web_page_preview=True
+                    ),
+                    reply_markup=reply_markup
+                )
+            )
+        except Exception as error:
+            print(error)
+    await update.answer(answers)
+
+
+Bot.run()
